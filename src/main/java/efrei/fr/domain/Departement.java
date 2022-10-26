1bffb6a5d764a59075de0000d8a9c5ca511387d9
sampdv <116738656+sampdv@users.noreply.github.com>
2022-10-26 17:04:12 +0200

    Add files via upload

---
+++ src/main/java/efrei/fr/domain/Departement.java
@@ -0,0 +1,29 @@
+public class Departement {
+
+    private double grade;
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public Departement(double grade) {
+        this.grade = grade;
+    }
+
+
+
+    public void printStudent(String studentNumber){
+
+    }
+    public void printTeacher(String staffNumber){
+
+
+
+    }
+
+
+}
